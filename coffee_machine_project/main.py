a09c94ea520e8b30eef2a39953ec90a95a873070
OvidioCalvet <calvetovidio@gmail.com>
2024-11-08 15:34:54 -0500

    created dictionaries

--- coffee_machine_project/main.py
+++ coffee_machine_project/main.py
@@ -1,1 +1,46 @@
-print('coffee machine project up and running')
+MENU: dict = {
+
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18
+        },
+
+        "Cost": 1.5,
+
+    },
+
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+
+        "Cost": 2.5,
+
+    },
+
+    "cappuccino": {
+        "ingredients": {
+            "water":250,
+            "milk":100,
+            "coffee":24,
+        },
+
+        "cost": 3.0,
+
+    }
+
+}
+
+resources: dict = {
+
+    "water": 300,
+    "milk": 200,
+    "coffee": 100
+
+}
+
+
+
